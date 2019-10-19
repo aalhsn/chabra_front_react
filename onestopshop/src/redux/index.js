76b50a2adf8ae28727177a8f4226207f2ae5fc2b
bashayer_nouri <bashayer_nouri@hotmail.com>
2019-10-19 19:10:49 +0300

    Fixes

--- onestopshop/src/redux/index.js
+++ onestopshop/src/redux/index.js
@@ -1,17 +1,13 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { checkForExpiredToken, fetchProducts} from "./actions";
+import { checkForExpiredToken, fetchProducts } from "./actions";
 import reducer from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 store.dispatch(fetchProducts());
-store.dispatch(checkForExpiredToken())
+store.dispatch(checkForExpiredToken());
 
 export default store;
